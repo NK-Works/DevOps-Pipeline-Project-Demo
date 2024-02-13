4bb402b8080ab09d059119376240f41699ff0311
NK-Works <nkworks777@gmail.com>
2024-02-13 19:35:03 +0530

    ArticleUpdate

--- src/NewPost.jsx
+++ src/NewPost.jsx
@@ -6,7 +6,7 @@ import './NewPost.css'  // Importing the NewPost css flie
 function NewPost() {
   return (
     <div className='myForm'>
-      <div className='myHeading'>New Post </div>
+      <div className='myHeading'>New Article </div>
     </div>
   )
 }
